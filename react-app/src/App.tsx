3cf081ed8ce6a98813eb3100bcd4314f83d98135
Ananth <ananthsoft@gmail.com>
2023-06-26 01:55:53 -0700

    React update the state as Asynchronously

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,25 @@
 import "./App.css";
-import { BsFillCalendarCheckFill } from "react-icons/bs";
-import Button from "./components/Button/Button";
+import { useState } from "react";
 
 function App() {
+  //React updates the state asynchronously meaning here the line no 13 setVisibility(true) is setting visibility value to true,
+  // but you can still see the concole.log(visibility) will display as false for the first time because it updates the state asynchronously
+  // Why because to have better performance, example if you try to set the many state using hooks due to many rerendering it will take lot of time,
+  // to avoid this scenario, react will create batches to update all the state in single short.
+
+  const [isVisibility, setVisibility] = useState(false);
+
+  const handleClick = () => {
+    setVisibility(true);
+    console.log(isVisibility);
+  };
+
   return (
     <div>
-      <BsFillCalendarCheckFill size="140" color="green" />
-      <Button
-        buttonColor="primary"
-        children="Fancy Button"
-        onClick={() => console.log("clicked")}
-      />
+      {/* <BsFillCalendarCheckFill size="140" color="green" /> */}
+      <button onClick={handleClick}>
+        React Update the state as Asynchronously
+      </button>
     </div>
   );
 }
